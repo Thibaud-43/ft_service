ede6b1d1fdc16cc74488da37ad7f080a50810c65
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2021-05-20 18:19:40 +0200

    ENTRYPOINT OK // TO DO : export SQL

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -4,4 +4,5 @@ echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET utf8 C
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -u root
 echo "GRANT ALL ON $MYSQL_DATABASE .* TO '$MYSQL_USER'@'%'" | mysql -u root
 echo "SHOW DATABASES" | mysql -u root
-tail -f /dev/null
+service mysql stop
+/usr/sbin/mysqld -u root
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,6 +1,7 @@
 sed -i -e "s|MYSQL_DATABASE|'$MYSQL_DATABASE'|g" ./tmp/wp-config.php
 sed -i -e "s|MYSQL_USER|'$MYSQL_USER'|g" ./tmp/wp-config.php
 sed -i -e "s|MYSQL_PASSWORD|'$MYSQL_PASSWORD'|g" ./tmp/wp-config.php
+sed -i -e "s|;daemonize = yes|daemonize = no|g" ./etc/php/7.3/fpm/php-fpm.conf
 mv ./tmp/wp-config.php ./www/
-service php7.3-fpm start
-tail -f /dev/null
+mkdir -p /run/php/
+/usr/sbin/php-fpm7.3
